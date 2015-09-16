7c35ace137614f595d4e6c29993f0de76cb262f9
Jason Rey <jasonrey@outlook.com>
2015-09-17 01:43:07 +0800

    WIP hamburger page

---
+++ templates/page/hamburger/content.php
@@ -0,0 +1,18 @@
+<?php
+!defined('SERVER_EXEC') && die('No access.');
+?>
+<div class="demo">
+	<div class="hamburger-items">
+		<div class="hamburger-menu hamburger-menu-1">
+			<div class="hamburger-menu-lines">
+				<span class="hamburger-menu-line"></span>
+			</div>
+		</div>
+
+		<div class="hamburger-menu hamburger-menu-2">
+			<div class="hamburger-menu-lines">
+				<span class="hamburger-menu-line"></span>
+			</div>
+		</div>
+	</div>
+</div>
