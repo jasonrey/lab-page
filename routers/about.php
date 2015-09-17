a461c66ef5afeab9236efea06aeb1f474496e817
Jason Rey <jasonrey@outlook.com>
2015-09-18 01:15:24 +0800

    WIP about page

---
+++ routers/about.php
@@ -0,0 +1,6 @@
+<?php
+!defined('SERVER_EXEC') && die('No access.');
+
+class AboutRouter extends Router
+{
+}
---
+++ templates/about/index.php
@@ -0,0 +1,39 @@
+<?php
+!defined('SERVER_EXEC') && die('No access.');
+?>
+<div id="about">
+	<div class="section section-header">
+		<div class="section-content">
+			<div class="section-background">
+				<div class="section-background-pattern"><?php echo str_repeat($backgroundPattern, 100); ?></div>
+				<div class="section-background-overlay"></div>
+			</div>
+			<div class="page-title">
+				<h2>jsonobject.io</h2>
+				<h1>Hello World</h1>
+			</div>
+		</div>
+	</div>
+	<div class="section section-intro">
+		<div class="container">
+			<div class="section-content">
+				<h3 class="name">Jason Rey</h3>
+				<h4 class="subtitle">A developer at heart, with a touch of musician.</h4>
+				<div class="social-links">
+					<a href="#" class="social-link"><i class="icon icon-facebook"></i></a>
+					<a href="#" class="social-link"><i class="icon icon-twitter"></i></a>
+					<a href="#" class="social-link"><i class="icon icon-linkedin"></i></a>
+					<a href="#" class="social-link"><i class="icon icon-github"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="section section-intro-2">
+		<div class="container">
+			<div class="section-content">
+				<p>People call me the <span class="highlight">"Jays"</span> of all trades.</p>
+				<p>As an Electronics Engineering student, I may not have a strict IT background, but I am a self-taught developer. Disciplined and independent, I learn as I go from projects that I have no prior knowledge. I do not believe in "by-the-book" programming. Logic is the key, while languages are secondary.</p>
+			</div>
+		</div>
+	</div>
+</div>
