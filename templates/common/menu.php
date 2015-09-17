c1478681af6b2be09a6003b96eb546be33d116d1
Jason Rey <jasonrey@outlook.com>
2015-09-17 16:16:31 +0800

    Updated menu

--- templates/common/menu.php
+++ templates/common/menu.php
@@ -11,7 +11,7 @@
 		</div>
 	</div>
 	<ul class="menu-items">
-		<li class="menu-item menu-item-index"><a class="menu-item-link <?php if ($viewname === 'index') { ?>active<?php } ?>" href="<?php echo Lib::url('index'); ?>">Home</a></li>
+		<li class="menu-item menu-item-index"><a class="menu-item-link <?php if ($viewname === 'index') { ?>active<?php } ?>" href="<?php echo Lib::url('index'); ?>">Index</a></li>
 		<?php foreach (Lib::view('page')->getPages() as $pageslug => $page) { ?>
 		<li class="menu-item"><a class="menu-item-link <?php if ($viewname === 'page' && $pageslug === $slug) { ?>active<?php } ?>" href="<?php echo Lib::url('page', array('slug' => $pageslug)); ?>"><?php echo $page->title; ?></a></li>
 		<?php } ?>
