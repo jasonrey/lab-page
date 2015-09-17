ca6941243523899b0c57173a4d70453cafad97f7
Jason Rey <jasonrey@outlook.com>
2015-09-17 11:18:44 +0800

    Added page base index

---
+++ templates/page/index.php
@@ -0,0 +1,8 @@
+<?php
+!defined('SERVER_EXEC') && die('No access.');
+?>
+<div id="<?php echo $slug; ?>" class="main">
+	<div class="container">
+		<?php echo $content; ?>
+	</div>
+</div>
--- views/page.php
+++ views/page.php
@@ -3,6 +3,9 @@
 
 class PageView extends View
 {
+	public $css = array();
+	public $js = array();
+
 	public function getPages()
 	{
 		static $posts;
@@ -23,5 +26,17 @@ class PageView extends View
 		$slug = Req::get('slug');
 
 		$this->set('slug', $slug);
+
+		$this->css[] = $slug;
+		$this->js[] = $slug;
+
+		$page = $this->getPages()->$slug;
+
+		$this->set('page', $page);
+		$this->set('pagetitle', $page->title);
+
+		$content = $this->loadTemplate($slug . '/content');
+
+		$this->set('content', $content);
 	}
 }
