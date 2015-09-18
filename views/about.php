ca7728fbf54ef86d28b16ae43cbc56cc405de918
Jason Rey <jasonrey@outlook.com>
2015-09-18 22:07:59 +0800

    Added Twitter feed in about page

--- views/about.php
+++ views/about.php
@@ -10,5 +10,11 @@ class AboutView extends View
 		$this->set('backgroundPattern', '1001000110010111011001101100110111110000010101111101111111001011011001100100');
 		$this->set('hideHeader', true);
 		$this->set('pagetitle', 'Hello World');
+
+		$twitterHelper = Lib::helper('twitter');
+
+		$tweet = $twitterHelper->getLatestTweet('jasonkeorey');
+
+		$this->set('tweet', $tweet);
 	}
 }
