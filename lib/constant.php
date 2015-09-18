2c53343ae7f7a274e7ccb4162c18d7a05b0d8e31
Jason Rey <jasonrey@outlook.com>
2015-09-18 22:07:50 +0800

    Added Twitter API helper

--- lib/constant.php
+++ lib/constant.php
@@ -1,3 +1,6 @@
 <?php
 
 !defined('SERVER_EXEC') && die('No access.');
+
+define('TWITTER_API_KEY', 'XIdDtGw4fiYBZqjFjbYLgINTb');
+define('TWITTER_API_SECRET', 'lGv7CewRu9XHk5L2VHyB1rWNKOtU9yYQmQTTtiAmMZY0jYt4ab');
